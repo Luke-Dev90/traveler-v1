104e1c25dfc1f1fa44947fe32f8601bf5175cec3
lucaschalela990 <lukeesf2016@gmail.com>
2021-11-19 10:08:43 -0300

    init project

---
+++ src/test/java/com/lchalela/pasajes/PasajesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lchalela.pasajes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PasajesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
